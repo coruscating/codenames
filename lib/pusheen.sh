06ad9cacf827b785ce09724ca5cddd98bfd8ebf2
halz <halz@mit.edu>
2016-03-23 13:40:12 -0400

    more pusheen

--- lib/pusheen.sh
+++ lib/pusheen.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+num=109
+
 echo "wordlist_pusheen = [" > wordlist_pusheen.js
 i=1
 while :
 do
 echo "{name: 'pusheen/pusheen"$i"t.gif'}," >> wordlist_pusheen.js
 i=`expr $i + 1`
-if [ $i -ge 65 ] ; then
+if [ $i -ge $num ] ; then
 echo "{name: 'pusheen/pusheen"$i"t.gif'}" >> wordlist_pusheen.js
 
 echo "];" >> wordlist_pusheen.js
