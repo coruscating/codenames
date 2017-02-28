c34be081ddbce8995095d18b7cccafeadc0062f7
halz <halz@mit.edu>
2017-02-28 18:10:16 -0500

    made regex symbols bigger, fixed alignment?

--- lib/wordconverter_regex.sh
+++ lib/wordconverter_regex.sh
@@ -7,10 +7,11 @@ if [ -z $filein ] ; then
 fi
 
 sort -u $filein -o $filein
-sed 's/\./\&#46;/g' $filein > $filein.tmp
-#sed -i 's/{/\&#123;/g' $filein.tmp
-#sed -i 's/}/\&#125;/g' $filein.tmp
+sed 's/\./\&#8226;/g' $filein > $filein.tmp
+sed 's/{/\&#123;/g' $filein.tmp > $filein.tmp2
+sed 's/}/\&#125;/g' $filein.tmp2 > $filein.tmp
 sed "s/'/\&#39;/g" $filein.tmp > $filein.tmp2
+sed 's/\*/\&#10033;/g' $filein.tmp2 > $filein.tmp
 
 echo "$filein = [" > $fileout
 
@@ -18,7 +19,7 @@ while read -r line
 do
     name=$line
     echo "{name: '$line'}," >> $fileout
-done < $filein.tmp2
+done < $filein.tmp
 
 
 sed -i '' '$s/,//' $fileout
